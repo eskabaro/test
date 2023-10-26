e234fb5d07026608e8f1f00ef7cfaf71937c8504
Sasha <kononucenkosasa4004@gmail.com>
2023-10-26 20:47:50 +0300

    Updates

---
+++ static/js/888.6b480913.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest=self.webpackChunktest||[]).push([[888],{38888:function(t,e,s){s.r(e);s(72791);var n=s(80184);e.default=function(){return(0,n.jsx)("table",{children:(0,n.jsx)("tbody",{children:(0,n.jsxs)("tr",{children:[(0,n.jsx)("td",{children:"Dummy"}),(0,n.jsx)("td",{children:"Chart"})]})})})}}}]);
+//# sourceMappingURL=888.6b480913.chunk.js.map
